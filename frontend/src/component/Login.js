04b1db05431d6329c95463aa19cf36b307b954ab
ShreyaDubey1001 <115868572+ShreyaDubey1001@users.noreply.github.com>
2023-11-24 20:36:33 +0530

    Update Login.js
    improved the design

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./login.css
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -34,55 +35,89 @@ const Login = () => {
 
   return (
     <>
-      <form>
-        <div className="form-group row">
-          <label htmlFor="inputEmail" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-5">
-            <input
-              type="text"
-              className="form-control"
-              id="inputEmail"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-        </div>
+      <section className="vh-100">
+        <div className="container-fluid h-custom">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-md-9 col-lg-6 col-xl-5">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="img-fluid"
+                alt="Sample image"
+              />
+            </div>
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+              <form>
+                {/* Email input */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="inputEmail"
+                    className="form-control form-control-lg"
+                    placeholder="Enter a valid email address"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                  <label className="form-label" htmlFor="inputEmail">
+                    Email address
+                  </label>
+                </div>
+                {/* Password input */}
+                <div className="form-outline mb-3">
+                  <input
+                    type="password"
+                    id="inputPassword"
+                    className="form-control form-control-lg"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                  <label className="form-label" htmlFor="inputPassword">
+                    Password
+                  </label>
+                </div>
 
-        <div className="form-group row">
-          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-5">
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
+                <div className="text-center text-lg-start mt-4 pt-2">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg"
+                    onClick={handleSubmit}
+                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-
-        <div className="form-group row">
-          <div className="col-sm-10">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={handleSubmit}
-            >
-              Login
-            </button>
+        <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+          {/* Copyright */}
+          <div className="text-white mb-3 mb-md-0">
+            Copyright © 2020. All rights reserved.
           </div>
+          {/* Copyright */}
+          {/* Right */}
+          <div>
+            <a href="#!" className="text-white me-4">
+              <i className="fab fa-facebook-f" />
+            </a>
+            <a href="#!" className="text-white me-4">
+              <i className="fab fa-twitter" />
+            </a>
+            <a href="#!" className="text-white me-4">
+              <i className="fab fa-google" />
+            </a>
+            <a href="#!" className="text-white">
+              <i className="fab fa-linkedin-in" />
+            </a>
+          </div>
+          {/* Right */}
         </div>
-      </form>
+      </section>
     </>
   );
 };
