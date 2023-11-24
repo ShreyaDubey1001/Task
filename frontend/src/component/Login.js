c63e14cc89388ea203d7626d13bf7402f8c3956c
ShreyaDubey1001 <115868572+ShreyaDubey1001@users.noreply.github.com>
2023-11-24 21:26:49 +0530

    Update Login.js

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css
+import "./login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
